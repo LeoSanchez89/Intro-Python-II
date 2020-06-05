c5395434f78cb24fcd6a487a953521432c858463
LeoSanchez89 <58442719+LeoSanchez89@users.noreply.github.com>
2020-06-04 20:30:39 -0400

    game complete!!

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,6 @@
 from room import Room
 from player import Player
+from item import Item
 
 # Declare all the rooms
 
@@ -36,14 +37,20 @@ room['treasure'].s_to = room['narrow']
 
 #
 # Main
-#
+#items in rooms
+
+# room['outside'].items = [Item("empty", "nothing to see here...")]
+room['foyer'].items = [Item("bandana", "A dusty, tattered, old red bandana.")]
+room['overlook'].items = [Item("flashlight", "Beat up, but still functional...it could be useful."), Item("sword", "It's definitely seen better days, but a sword is a sword.")]
+room['narrow'].items = [Item("coins", "Looks like somebody dropped a few gold coins in a hurry...")]
+room['treasure'].items = [Item("bones", "Someone made off with the treasure...but at what cost?")]
 
 # Make a new player object that is currently in the 'outside' room.
 name = None
 while not name:
     name = input("PLease Enter Your Name: ")
 me = Player(name, "outside")
-
+print(f'\n Welcome, {me.name.capitalize()}! Your adventure begins!')
 # Write a loop that:
 #
 # * Prints the current room name
@@ -62,13 +69,81 @@ while not end_game:
     #         if key == me.current_room:
     #             print(f'Current Location: {value.name}.\n "{value.description}"')
     # room_info(room)
-    print(f'\n Current Location: {room[me.current_room].name}.\n "{room[me.current_room].description}"')
+
+    print(f'\n Current Location: {room[me.current_room].name}.\n \n"{room[me.current_room].description}"')
+
+    # item search
+    search = True
+    while search:
+
+        browse = input('\n Search room for items? (y/n) [or i to access inventory]: ')
+
+        if browse == "y" and room[me.current_room].items != None and room[me.current_room].items:
+            for item in room[me.current_room].items:
+                print(f'\n You look around and find: {item.name} - {item.description}\n')
+            # logic for picking up items
+            pickup = True
+            while pickup:
+                get_item = input('Enter "get/take [item name]" to pickup item, or c to cancel: ')
+                action = get_item.lower().split(" ")
+                if action[0] == "c":
+                    pickup = False
+                elif action[0] == "get" or action[0] == "take":
+                    found = False
+                    for item in room[me.current_room].items:
+                        if action[1] == item.name:
+                            found = True
+                            me.inventory.append(item)
+                            room[me.current_room].items.remove(item)
+                            print(f'\n You picked up "{action[1]}!"\n')
+                    if found == False:
+                        print("\n Item could not be found. Try again.\n")
+                else:
+                    # pickup = False
+                    print('\n Invalid Input.\n')        
+            # search = False 
+        elif browse == "i":
+            if me.inventory:
+                check_inv = True
+                while check_inv:
+                    print('\n INVENTORY:')
+                    for item in me.inventory:
+                        print(f'\n{item.name} - {item.description}')
+                    item_drop = input('\n Enter "drop [item name]" to drop item, or c to cancel: ')
+                    drop_action = item_drop.lower().split(" ")
+                    if drop_action[0] == "c":
+                        check_inv = False
+                    elif drop_action[0] == "drop":
+                        found = False
+                        for item in me.inventory:
+                            if drop_action[1] == item.name:
+                                found = True
+                                me.inventory.remove(item)
+                                room[me.current_room].items.append(item)
+                                print(f'\n You dropped "{drop_action[1]}!"\n')
+                        if found == False:
+                            print("\n Item could not be found. Try again.\n")
+                    else:
+                         print('\n Invalid Input.\n')
+            else:
+                print('\n There are no items in your inventory.\n')
+                check_inv = False
+        elif browse == "n":
+            search = False
+        elif room[me.current_room].items == None or not room[me.current_room].items and browse == "y":
+            print(f'\n You look around, but find nothing...')
+            search = False
+        else:
+            print("\n Invalid input. Please try again")
+
+    # move in a direction
     move = False
     while not move:
-
+        
         make_move = input("\n Enter a direction to move in (n, s, e, w) or enter q to quit: ")
         
         if make_move == "q":
+            print("\n Game Over!!!")
             end_game = True
             move = True
         elif make_move != "n" and make_move != "s" and make_move != "e" and make_move != "w":
